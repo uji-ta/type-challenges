58bf5f258b8f200c90dc2e614d614e57a2c2a7b5
uji-ta <kmnswimert@gmail.com>
2024-10-01 00:46:09 +0900

    :fix: type

---
+++ 11-TupletoObject.ts
@@ -0,0 +1,32 @@
+/* _____________ ここにコードを記入 _____________ */
+
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  [P in T[number]]: P;
+};
+
+/* _____________ テストケース _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const tupleNumber = [1, 2, 3, 4] as const
+const sym1 = Symbol(1)
+const sym2 = Symbol(2)
+const tupleSymbol = [sym1, sym2] as const
+const tupleMix = [1, '2', 3, '4', sym1] as const
+
+type cases = [
+  Expect<Equal<TupleToObject<typeof tuple>, { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y' }>>,
+  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1, 2: 2, 3: 3, 4: 4 }>>,
+  Expect<Equal<TupleToObject<typeof tupleSymbol>, { [sym1]: typeof sym1, [sym2]: typeof sym2 }>>,
+  Expect<Equal<TupleToObject<typeof tupleMix>, { 1: 1, '2': '2', 3: 3, '4': '4', [sym1]: typeof sym1 }>>,
+]
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>
+
+/* _____________ 次のステップ _____________ */
+/*
+  > 解答を共有する：https://tsch.js.org/11/answer/ja
+  > 解答を見る：https://tsch.js.org/11/solutions
+  > その他の課題：https://tsch.js.org/ja
+*/
